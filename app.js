1c04b876eb7135ede53cfbaeb7d2fa9a9317e754
Suyeon Kang <tndusrkd92@gmail.com>
2020-03-09 18:29:25 +0900

    Set up a module pattern

---
+++ app.js
@@ -0,0 +1,28 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+
+    // Some code
+
+})();
+
+
+
+
+// UI CONTROLLER
+var UIControllor = (function() {
+
+    // Some code 
+
+})();
+
+
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    // Some code
+
+})(budgetController, UIControllor);
+
